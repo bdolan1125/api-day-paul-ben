3c8d16d8adc4bb39e31895068ccbfb8575754685
Benjamin Dolan <benjamin@thinkful.com>
2020-01-16 12:16:43 -0700

    finished exercise 1-5

--- src/index.js
+++ src/index.js
@@ -2,10 +2,17 @@ import $ from 'jquery';
 
 import 'normalize.css';
 import './index.css';
-
+import store from './store.js';
+import api from './api';
 import shoppingList from './shopping-list';
 
 const main = function () {
+  api.getItems()
+  .then(res => res.json())
+  .then((items) => {
+    items.forEach((item) => store.addItem(item));
+    shoppingList.render();
+  });
   shoppingList.bindEventListeners();
   shoppingList.render();
 };
